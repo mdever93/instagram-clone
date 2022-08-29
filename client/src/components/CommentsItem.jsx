662429d1827a0e81142dd1f188c335308a03ccc9
Michael Dever <mdever@live.ca>
2022-08-29 16:37:06 -0400

    reply component layout complete

--- client/src/components/CommentsItem.jsx
+++ client/src/components/CommentsItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Reply from './Reply';
 import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'; import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 
 
@@ -6,19 +7,26 @@ import './CommentsItem.scss'
 
 const CommentsItem = () => {
   return (
-    <div className='comments-item flex'>
-      <img className='avatar' src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
-      <div className='column'>
-      <p className='content'><span className='bold'>Username</span> Caption goes here Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum vel velit nemo, distinctio possimus quisquam non delectus corrupti vitae rerum fugiat! Rem rerum ipsum minima illo culpa error doloribus.</p>
-      <span className='time-since-posted content grey'>10d</span>
-      <span className='comment-likes content grey bold'>15 likes</span>
-      <span className='reply content grey bold'>Reply</span>
-      <span className='send content grey bold'>Send</span>
+    <>
+      <div className='comments-item flex'>
+        <img className='avatar' src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <div className='column'>
+          <div className='row flex'>
+            <p className='content'><span className='bold'>Username</span> Caption goes here Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum vel velit nemo, distinctio possimus quisquam non delectus corrupti vitae rerum fugiat! Rem rerum ipsum minima illo culpa error doloribus.</p>
+            <div className='like-button flex grey'>
+              <FavoriteBorderRoundedIcon fontSize='inherit' />
+            </div>
+          </div>
+          <span className='time-since-posted content grey'>10d</span>
+          <span className='comment-likes content grey bold'>15 likes</span>
+          <span className='reply content grey bold'>Reply</span>
+          <span className='send content grey bold'>Send</span>
+          <Reply />
+          <Reply />
+          <Reply />
+        </div>
       </div>
-      <div className='like-button flex grey'>
-        <FavoriteBorderRoundedIcon fontSize='inherit' />
-      </div>
-    </div>
+    </>
   )
 }
 
--- client/src/components/Reply.jsx
+++ client/src/components/Reply.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'
 
 import './Reply.scss'
 
 const Reply = () => {
   return (
-    <div>Reply</div>
-  )
+    <div className='reply-item flex'>
+    <img className='avatar small' src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+    <div className='column'>
+    <div className='row flex'>
+            <p className='content'><span className='bold'>Username</span> Caption goes here Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam cum vel velit nemo, distinctio possimus quisquam non delectus corrupti vitae rerum fugiat! Rem rerum ipsum minima illo culpa error doloribus.</p>
+            <div className='like-button flex grey'>
+              <FavoriteBorderRoundedIcon fontSize='inherit' />
+            </div>
+          </div>
+      <span className='time-since-posted content grey'>10d</span>
+      <span className='comment-likes content grey bold'>15 likes</span>
+      <span className='reply content grey bold'>Reply</span>
+      <span className='send content grey bold'>Send</span>
+    </div>
+  </div>
+)
 }
 
 export default Reply
