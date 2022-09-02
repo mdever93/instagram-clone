2277fe9a7bdd9dd895d9a90e00e92bb9d6fcc64e
Michael Dever <mdever@live.ca>
2022-09-02 03:26:02 -0400

    created express server

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 
 import './LogIn_SignUp.scss'
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className='signup flex'>
       <img className='logo' src="https://www.pngkey.com/png/full/1-13459_instagram-font-logo-white-png-instagram-white-text.png" alt="" />
       <p className='tagline bold'>Sign up to see photos and videos from your friends.</p>
-      <input type="email" name="email" id="email" placeholder='email' />
-      <input className='username' type="text" name="username" id="username" placeholder='username' />
-      <input className='password' type="password" name="password" id="password" placeholder='password' />
+      <input
+        className='email'
+        type="email"
+        placeholder='email'
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value)
+        }}
+      />
+      <input
+        className='username'
+        type="text"
+        placeholder='username'
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value)
+        }}
+      />
+      <input
+        className='password'
+        type="password"
+        placeholder='password'
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
+      />
       <button className='login_btn bold' type="submit">Log in</button>
       <div className='footer'>
         <p> Already have an account? <span className='bold'>Sign In.</span></p>
---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const app = require('express')();
+const express = require('express');
+const server = require('http').createServer(app);
+const port = process.env.PORT || 8080;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+const { Pool } = require('pg');
+
+app.use(cors({ origin: `*`, credentials: true }));
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(morgan('dev'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['2FCF9B2F-5DD0-4EEE-B964-D00E916CA634', 'B01EB1F4-19A7-4F97-B0AC-C979174408A4']
+}));
+
+const db = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+db.connect()
+  .then(() => console.log('db connected'))
+  .catch(err => console.error('db connection error', err.stack));
