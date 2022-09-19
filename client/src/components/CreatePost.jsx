befc9962603360329ea07271f70bfe4b130f493a
Michael Dever <mdever@live.ca>
2022-09-19 01:02:12 -0400

    users can now add a caption after uploading a photo

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -3,12 +3,15 @@ import { useState } from 'react';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
 
 
 import './CreatePost.scss'
 
 const CreatePost = (props) => {
   const [uploadedImage, setUploadedImage] = useState(null)
+  const [showCaptionInput, setShowCaptionInput] = useState(false)
+  const [caption, setCaption] = useState('')
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -20,26 +23,62 @@ const CreatePost = (props) => {
       setUploadedImage(readerEvent.target.result);
     }
   }
+
+  const next = () => {
+    if (uploadedImage) {
+      setShowCaptionInput(true);
+    }
+  }
+
+  const back = () => {
+    setShowCaptionInput(false)
+  }
+
+  const removeImage = () => {
+    setUploadedImage(null)
+    if (caption) {
+      setCaption('')
+    }
+  }
   return (
     <div className='create-post flex'>
       <div className='header flex'>
-        <CloseRoundedIcon className='close' onClick={props.timeline} fontSize='inherit' />
+        {!showCaptionInput && <CloseRoundedIcon className='close' onClick={props.timeline} fontSize='inherit' />}
+        {showCaptionInput && <ArrowBackIosRoundedIcon className='back' onClick={() => back()} fontSize='inherit' />}
         <p className='bold'>New post</p>
-        <p className='bold next'>Next</p>
+        {!showCaptionInput && <p className='bold next' onClick={() => next()}>Next</p>}
+        {showCaptionInput && <p className='bold next' onClick={() => console.log(caption)}>Share</p>}
       </div>
-      <div className='add-photo flex'>
-        {uploadedImage ? (
-          <>
-            <img src={uploadedImage} alt="" />
-            <HighlightOffRoundedIcon onClick={() => setUploadedImage(null)} fontSize='inherit' />
-          </>
-        ) : (
-          <label>
-            <input type="file" hidden onChange={addImageToPost} />
-            <AddRoundedIcon fontSize='inherit' />
-          </label>
-        )}
-      </div>
+      {showCaptionInput ? (
+        <div className='caption-section flex'>
+          <img src={uploadedImage} alt="" />
+          <textarea
+            className='caption'
+            type="text"
+            placeholder='Write a caption...'
+            value={caption}
+            onChange={(e) => {
+              setCaption(e.target.value)
+            }}
+          />
+        </div>
+      ) : (
+
+
+        <div className='add-photo flex'>
+          {uploadedImage ? (
+            <>
+              <img src={uploadedImage} alt="" />
+              <HighlightOffRoundedIcon onClick={() => removeImage()} fontSize='inherit' />
+            </>
+          ) : (
+            <label>
+              <input type="file" hidden onChange={addImageToPost} />
+              <AddRoundedIcon fontSize='inherit' />
+            </label>
+          )}
+        </div>
+      )}
     </div >
   )
 }
