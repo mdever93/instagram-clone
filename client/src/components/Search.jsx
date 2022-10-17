a97155d57fcead6937aed0c623bfda84b1e2ef6a
Michael Dever <mdever@live.ca>
2022-10-16 22:11:17 -0400

    search page design complete

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+
+
+import './Search.scss'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search-page flex'>
+      <div className='header flex'>
+        <SearchRoundedIcon />
+        <input type="text" name="" id="" placeholder='Search' className='search' />
+      </div>
+      <div className='suggested-posts'>
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+        <img src="https://avatars.dicebear.com/api/pixel-art/1002.svg" alt="" />
+
+      </div>
+    </div>
   )
 }
 
